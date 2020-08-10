eed516d176c93070cf1df6cca865e7ab1a1c01ce
gsuryatejaiam-at-043758670547 <gsuryateja2265@gmail.com>
2020-08-10 21:04:42 +0530

    config.js modified

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-2_Mhry6kdQ5', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '301c2b59pn63egrpukikim6uor', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-2' // e.g. us-east-2
+        userPoolId: 'us-east-2_Mhry6kdQ5',
+        userPoolClientId: '301c2b59pn63egrpukikim6uor',
+        region: 'us-east-2'
     },
     api: {
-        invokeUrl: 'https://v461rd4a22.execute-api.us-east-2.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://v461rd4a22.execute-api.us-east-2.amazonaws.com/prod'
     }
 };
